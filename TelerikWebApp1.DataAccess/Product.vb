dd1e556efaf311c9ea9566e1643490000e8e103b
Full Metal <tyler9725@gmail.com>
2014-06-30 22:42:06 -0500

    //initial commit

---
+++ TelerikWebApp1.DataAccess/Product.vb
@@ -0,0 +1,33 @@
+﻿Public Class Product
+    Private _iD As Integer
+    Public Property ID() As Integer
+        Get
+            Return _iD
+        End Get
+        Set(ByVal value As Integer)
+            _iD = value
+        End Set
+    End Property
+
+
+    Private _productName As String
+    Public Property ProductName() As String
+        Get
+            Return _productName
+        End Get
+        Set(ByVal value As String)
+            _productName = value
+        End Set
+    End Property
+
+
+    Private _price As Decimal
+    Public Property Price() As Decimal
+        Get
+            Return _price
+        End Get
+        Set(ByVal value As Decimal)
+            _price = value
+        End Set
+    End Property
+End Class
---
+++ TelerikWebApp1/index.aspx.vb
@@ -0,0 +1,7 @@
+﻿Imports Telerik.Web.UI
+Imports System.Data
+
+Partial Class Index
+    Inherits System.Web.UI.Page
+
+End Class
